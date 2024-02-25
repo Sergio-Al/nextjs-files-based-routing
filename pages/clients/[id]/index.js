5d317fccb4c3de183afea92f66872a3b095edc44
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-02-25 11:15:34 -0400

    Feat. navigating programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 function ClientProjectsPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    console.log(router.pathname);
-    console.log(router.query);
-    
-    return (
-        <div>
-            <h1>The projects of a given client</h1>
-        </div>
-    );
+  console.log(router.pathname);
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/sergioid/projecta");
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "sergioid", clientProjectId: "projecta" },
+    });
+  }
+
+  return (
+    <div>
+      <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
+    </div>
+  );
 }
 
 export default ClientProjectsPage;
