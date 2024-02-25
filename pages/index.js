c23a95c3262c0d9149c0223aacc87b37ed1ff70b
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-02-24 22:56:22 -0400

    Feat. Adding a first page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// this is the main component for '/'
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>The home page!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
